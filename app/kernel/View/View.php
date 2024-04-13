100ad3834f9480fa71c51469c6cbf31430146d61
Vadim Semeniuc <semeniuc@int.pl>
2024-04-13 15:13:26 +0200

    added exception ViewNotFound

--- app/kernel/View/View.php
+++ app/kernel/View/View.php
@@ -4,20 +4,35 @@ declare(strict_types=1);
 
 namespace App\Kernel\View;
 
+use App\Kernel\Exceptions\ViewNotFoundException;
+
 class View
 {
+    /**
+     * @throws ViewNotFoundException
+     */
     public function page(string $name, array $data = []): void
     {
-        extract([
-            'view' => $this
-        ]);
-        
-        include_once APP_PATH . '/views/pages/' . $name . '.php';
+        $path = APP_PATH . "/views/pages/$name.php";
+
+        if (!file_exists($path)) {
+            throw new ViewNotFoundException(message: "View $name not found");
+        }
+
+        extract(['view' => $this]);
+        include_once $path;
     }
 
+    /**
+     * @throws ViewNotFoundException
+     */
     public function component(string $name, array $data = []): void
     {
+        $path = APP_PATH . "/views/components/$name.php";
+        if (!file_exists($path)) {
+            throw new ViewNotFoundException(message: "Component $name not found");
+        }
         extract($data);
-        include_once APP_PATH . '/views/components/' . $name . '.php';
+        include_once $path;
     }
 }
